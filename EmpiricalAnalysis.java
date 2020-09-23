bd0efb9b1825ec23ae240304e6847ff3ba593a6b
katherinetvl <63134770+katherinetvl@users.noreply.github.com>
2020-09-23 11:11:35 -0400

    Add files via upload

--- EmpiricalAnalysis.java
+++ EmpiricalAnalysis.java
@@ -4,7 +4,6 @@
 * Description: Empirical Analysis Exploration
 ********************************************/
 import java.lang.Math;      // for random() function 
-import java.util.Arrays;    // for returning arr in function
 
 public class EmpiricalAnalysis 
 {
@@ -19,11 +18,125 @@ public class EmpiricalAnalysis
         }
         */
 
+        // Generate 20 arrays 
         int[] arrAA = new int[1000];
-        // Generate random array 
         for (int a = 0; a < arrAA.length; a++)
         {
             arrAA[a] = RandomInt(1000);
+        }
+
+        int[] arrAB = new int[2000];
+        for (int b = 0; b < arrAB.length; b++)
+        {
+            arrAB[b] = RandomInt(2000);
+        }
+
+        int[] arrAC = new int[3000];
+        for (int c = 0; c < arrAC.length; c++)
+        {
+            arrAC[c] = RandomInt(3000);
+        }
+
+        int[] arrAD = new int[4000];
+        for (int d = 0; d < arrAD.length; d++)
+        {
+            arrAD[d] = RandomInt(4000);
+        }
+
+        int[] arrAE = new int[5000];
+        for (int e = 0; e < arrAE.length; e++)
+        {
+            arrAE[e] = RandomInt(5000);
+        }
+
+        int[] arrAF = new int[6000];
+        for (int f = 0; f < arrAF.length; f++)
+        {
+            arrAF[f] = RandomInt(6000);
+        }
+
+        int[] arrAG = new int[7000];
+        for (int g = 0; g < arrAG.length; g++)
+        {
+            arrAG[g] = RandomInt(7000);
+        }
+
+        int[] arrAH = new int[8000];
+        for (int h = 0; h < arrAH.length; h++)
+        {
+            arrAH[h] = RandomInt(8000);
+        }
+
+        int[] arrAI = new int[9000];
+        for (int ii = 0; ii < arrAI.length; ii++)
+        {
+            arrAI[ii] = RandomInt(9000);
+        }
+
+        int[] arrAJ = new int[10000];
+        for (int j = 0; j < arrAJ.length; j++)
+        {
+            arrAJ[j] = RandomInt(10000);
+        }
+
+        int[] arrAK = new int[11000];
+        for (int k = 0; k < arrAK.length; k++)
+        {
+            arrAK[k] = RandomInt(11000);
+        }
+
+        int[] arrAL = new int[12000];
+        for (int l = 0; l < arrAL.length; l++)
+        {
+            arrAL[l] = RandomInt(12000);
+        }
+
+        int[] arrAM = new int[13000];
+        for (int m = 0; m < arrAM.length; m++)
+        {
+            arrAM[m] = RandomInt(13000);
+        }
+
+        int[] arrAN = new int[14000];
+        for (int n = 0; n < arrAN.length; n++)
+        {
+            arrAN[n] = RandomInt(14000);
+        }
+
+        int[] arrAO = new int[15000];
+        for (int o = 0; o < arrAO.length; o++)
+        {
+            arrAO[o] = RandomInt(15000);
+        }
+
+        int[] arrAP = new int[16000];
+        for (int p = 0; p < arrAP.length; p++)
+        {
+            arrAP[p] = RandomInt(16000);
+        }
+
+        int[] arrAQ = new int[17000];
+        for (int q = 0; q < arrAQ.length; q++)
+        {
+            arrAQ[q] = RandomInt(17000);
+        }
+
+        int[] arrAR = new int[18000];
+        for (int r = 0; r < arrAR.length; r++)
+        {
+            arrAR[r] = RandomInt(18000);
+        }
+
+        int[] arrAS = new int[19000];
+        for (int s = 0; s < arrAS.length; s++)
+        {
+            arrAS[s] = RandomInt(19000);
+        }
+
+        int[] arrAT = new int[20000];
+        for (int t = 0; t < arrAT.length; t++)
+        {
+            arrAT[t] = RandomInt(20000);
         }
 
         /*
@@ -38,12 +151,50 @@ public class EmpiricalAnalysis
         // Invoke SortAnalysis for random array 
         // int result = SortAnalysis(arr1);
         int resultAA = SortAnalysis(arrAA);
+        int resultAB = SortAnalysis(arrAB);
+        int resultAC = SortAnalysis(arrAC);
+        int resultAD = SortAnalysis(arrAD);
+        int resultAE = SortAnalysis(arrAE);
+        int resultAF = SortAnalysis(arrAF);
+        int resultAG = SortAnalysis(arrAG);
+        int resultAH = SortAnalysis(arrAH);
+        int resultAI = SortAnalysis(arrAI);
+        int resultAJ = SortAnalysis(arrAJ);
+        int resultAK = SortAnalysis(arrAK);
+        int resultAL = SortAnalysis(arrAL);
+        int resultAM = SortAnalysis(arrAM);
+        int resultAN = SortAnalysis(arrAN);
+        int resultAO = SortAnalysis(arrAO);
+        int resultAP = SortAnalysis(arrAP);
+        int resultAQ = SortAnalysis(arrAQ);
+        int resultAR = SortAnalysis(arrAR);
+        int resultAS = SortAnalysis(arrAS);
+        int resultAT = SortAnalysis(arrAT);
 
         // Output size and SortAnalysis count
         System.out.println("Randomly generated int arrays:");
         System.out.println("n " + "\t" + "M ");
         // System.out.println(arr1.length + "\t" + result);
         System.out.println(arrAA.length + "\t" + resultAA);
+        System.out.println(arrAB.length + "\t" + resultAB);
+        System.out.println(arrAC.length + "\t" + resultAC);
+        System.out.println(arrAD.length + "\t" + resultAD);
+        System.out.println(arrAE.length + "\t" + resultAE);
+        System.out.println(arrAF.length + "\t" + resultAF);
+        System.out.println(arrAG.length + "\t" + resultAG);
+        System.out.println(arrAH.length + "\t" + resultAH);
+        System.out.println(arrAI.length + "\t" + resultAI);
+        System.out.println(arrAJ.length + "\t" + resultAJ);
+        System.out.println(arrAK.length + "\t" + resultAK);
+        System.out.println(arrAL.length + "\t" + resultAL);
+        System.out.println(arrAM.length + "\t" + resultAM);
+        System.out.println(arrAN.length + "\t" + resultAN);
+        System.out.println(arrAO.length + "\t" + resultAO);
+        System.out.println(arrAP.length + "\t" + resultAP);
+        System.out.println(arrAQ.length + "\t" + resultAQ);
+        System.out.println(arrAR.length + "\t" + resultAR);
+        System.out.println(arrAS.length + "\t" + resultAS);
+        System.out.println(arrAT.length + "\t" + resultAT);
 
         /*
         // Print random array after SortAnalysis 
@@ -115,9 +266,9 @@ Descending arrays
     public static int SortAnalysis(int arr[])
     {
         int count = 0; 
-        int n = arr.length;
+        int end = arr.length;
 
-        for (int i = 1; i < n - 1; i++)
+        for (int i = 1; i < end - 1; i++)
         {
             int v = arr[i];
             int j = i - 1;
