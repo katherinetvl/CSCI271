a2aba2a01df64890e981c2b4281597d63a37e0fd
katherinetvl <63134770+katherinetvl@users.noreply.github.com>
2020-10-18 19:53:49 -0400

    Update WordFind.java
    PGM will output a 2D char array now.

--- WordFind.java
+++ WordFind.java
@@ -1,6 +1,7 @@
 import java.io.File; // For File use 
 import java.io.IOException; // For File errors
 import java.util.Scanner; // For File reading 
+// import java.util.regex.*; // For String replacement 
 
 public class WordFind
 {
@@ -13,18 +14,27 @@ public class WordFind
         if (args.length > 0)
         {
             File gridFile = new File(args[0]);
-            String strCurr = null;
+            String strCurr = null; 
+            String tempChange = null; 
+            String strAlpha = "";
+            // String strRev = null;
 
             try 
             {
                 Scanner fin = new Scanner(gridFile);
                 while (fin.hasNextLine())
                 {
-                    // rowSize++;
                     strCurr = fin.nextLine();
-                    rowSize++;
-                    colSize++;
-                    System.out.println(strCurr);
+                    tempChange = strCurr.replaceAll("[^A-Za-z]+", "");
+                    
+                    // Increment row size if line has alphabet chars 
+                    if ((tempChange.matches("[A-Za-z]+")))
+                    {
+                        rowSize++;
+                    }
+                    strAlpha = AlphaOnlyString(strAlpha, strCurr);
+                    // System.out.println(strCurr);
+                    // System.out.println(strAlpha);
                 }
 
             // Close file
@@ -33,6 +43,35 @@ public class WordFind
             {
                 e.printStackTrace();
                 System.out.println("An error occurred with opening grid file.");
+            }
+
+            // Determine column size
+            int totalElements = strAlpha.length(); 
+            colSize = totalElements/rowSize; 
+            // System.out.println("Row size: " + rowSize + " Col size: " + colSize + "\n");
+            
+            // Build 2D char array from string of all alpha chars from grid 
+            char[] charArr1D = strAlpha.toCharArray();
+
+            char[][] wordGrid;
+            wordGrid = new char[rowSize][colSize];
+
+            int increment = 0;
+            for(int i = 0; i < rowSize; i++)
+            {
+                for(int j = 0; j < colSize; j++)
+                {
+                    if(increment == charArr1D.length)
+                    {
+                        break;
+                    } 
+
+                    wordGrid[i][j] = charArr1D[increment];
+                    System.out.print(wordGrid[i][j] + " ");
+
+                    increment++;
+                }
+                System.out.print("\n");
             }
 
             if (args.length > 1)
@@ -46,15 +85,15 @@ public class WordFind
                     while (fin2.hasNextLine())
                     {
                         strCurr2 = fin2.nextLine();
-                        System.out.println(strCurr2);
+                        // System.out.println(strCurr2);
                     }
 
                 fin2.close();
-                } catch (IOException e) // end of try 
+                } catch (IOException e) 
                 {
                     e.printStackTrace();
                     System.out.println("An error occurred with opening word(s) to search file.");
-                } // end for catch
+                }
             }
             else 
             {
@@ -65,18 +104,18 @@ public class WordFind
                     Scanner input = new Scanner(System.in);
                     while (input.hasNextLine())
                     {
-                        String wordToSearch = input.next();
-                        String wordToSearchLower = wordToSearch.toLowerCase(); 
-                        if(!(wordToSearchLower.equals("quit")))
+                        String wordIn = input.next();
+                        String wordCaps = wordIn.toUpperCase(); 
+                        if(!(wordCaps.equals("QUIT")))
                         {
-                            System.out.println("Word To Search is: " + wordToSearchLower + "\n");
+                            System.out.println("Word To Search is: " + wordCaps + "\n");
                         }
                         else 
                         {
                             System.exit(0);
                         }
                     }
-                // input.close();
+                input.close();
                 } catch(Exception e)
                     {
                         e.printStackTrace();
@@ -92,24 +131,33 @@ public class WordFind
         }
 
     }
-}
 
 // Functions 
 /**********************************************
 * Function Name: AlphaOnlyString
 * Description: Building a string of just alpha characters 
-* Input: StringExisting, FileString
-* Output: StringExisting with only alpha chars of FileString concatenated on
-**********************************************
-public static void ScanWordGrid(Char[] allChars, String fileLine) 
-{
+* Input: string1, string2
+* Output: string1 with alphabetical chars from string appended
+**********************************************/
+    public static String AlphaOnlyString(String finalString, String fileString) 
+    {
+        fileString = fileString.replaceAll("[^A-Za-z]+", "");
+        finalString = finalString.concat(fileString);
+        return finalString;
+    }
 
-} */
-
-// Obtain row and col (i and j values)
-
-// Convert string to char array 
-
-// Build word grid 
-
-// Search grid for pattern 
+/*******************************************************
+* Function Name: ReverseString
+* Description: A recursive function that reverses characters in string
+* Input: string
+* Output: string with reversed characters 
+**********************************************************/
+    public static String reverseString(String x)
+    {
+        if (x.isEmpty())
+        {
+            return x;
+        }
+        return reverseString(x.substring(1)) + x.charAt(0);
+    }
+}
